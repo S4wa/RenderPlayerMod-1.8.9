eb10f4c918b4894dfc81e9af92439c73f338cbf7
さわです <s4wa@outlook.com>
2018-03-07 18:32:02 +0900

    追加と変更
    サイズを変更できるようにした。

--- net/simplyrin/renderplayermod/RenderPlayerMod.java
+++ net/simplyrin/renderplayermod/RenderPlayerMod.java
@@ -25,7 +25,7 @@ import net.simplyrin.renderplayermod.command.RenderPlayer;
 @Mod(modid = RenderPlayerMod.MODID, version = RenderPlayerMod.VERSION, clientSideOnly = true)
 public class RenderPlayerMod {
 	public static final String MODID = "RenderPlayerMod";
-	public static final String VERSION = "0.0.3 PRE-RELEASE";
+	public static final String VERSION = "0.0.4 PRE-RELEASE";
 	private final Minecraft mc;
 
 	public RenderPlayerMod() {
